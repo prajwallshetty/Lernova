94cad56fdb40d22924c8c69c55494c9ab5ca8be2
Thanvi shetty <shettythanvi036@gmail.com>
2025-07-17 15:54:42 +0530

    Update page.tsx

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,6 +1,5 @@
-import React from 'react'
 
-const page = () => {
+const CompanionsLibrary = () => {
   return (
     <div>
       companions Library
@@ -8,4 +7,4 @@ const page = () => {
   )
 }
 
-export default page
+export default CompanionsLibrary
