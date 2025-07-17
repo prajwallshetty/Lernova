901d3dd3122de9ac2f47945eb82246183d7b2a48
prajwallshetty <prajwal0shetty11@gmail.com>
2025-07-17 15:35:39 +0530

    setting up app page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <div>
+      <h1 className="text-2xl">Welcome to my SaaS App</h1>
+      <Button>Click Me</Button>
+    </div>
   )
 }
 
