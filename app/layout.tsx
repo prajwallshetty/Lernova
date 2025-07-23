0161830750170072ca313acf453ac86b934d7bd7
prajwallshetty <prajwal0shetty11@gmail.com>
2025-07-23 21:09:44 +0530

    done with clerk authntiation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
@@ -21,8 +22,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${bricolage.variable} antialiased`}>
+         <ClerkProvider appearance={{ variables: { colorPrimary: '#fe5933' }} }>
         <Navbar />
         {children}
+        </ClerkProvider>
       </body>
     </html>
   );
