53b8662b674dc92d202f227dc45afbaf943bd847
Pahima R Uchil <uchilpahi@gmail.com>
2025-07-23 11:12:51 +0530

    Home and Companion Builder frontend

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,51 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList';
+import Cta from '@/components/CTA';
 import { Button } from '@/components/ui/button'
 import React from 'react'
+import { recentSessions } from '@/constants';
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl">Welcome to my SaaS App</h1>
-      <Button>Click Me</Button>
-    </div>
-  )
+    <main>
+      <h1 className="text-2xl">Popular Companions</h1>
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="456"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          subject="maths"
+          duration={30}
+          color="#e5d0ff"
+        />
+        <CompanionCard
+          id="789"
+          name="Verba: Smart Word Builder"
+          topic="English Literature"
+          subject="language"
+          duration={30}
+          color="#BDE7FF"
+        />
+      </section>
+
+      <section className="home-section">
+        <CompanionsList
+          title="Recently completed sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
+        <Cta />
+      </section>
+    </main>
+  );
 }
 
 export default Page
